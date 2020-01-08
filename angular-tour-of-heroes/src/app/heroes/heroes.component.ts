bba8d772055921b69062193975df87e6e7d3a2d6
Federico Morales <federico.morales@neoris.com>
2020-01-08 15:49:13 -0300

    Tutorial Angular – 3. El editor de Héroes

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+/* Cada componente debe declararse en sólo un NgModule. */
+
+import { Component, OnInit } from '@angular/core';
+import{Hero} from '../hero';
+
+@Component({
+  // El elemento selector, 'app-heroes', es el nombre del elemento HTML que identifica este componente 
+  // dentro de la plantilla del componente padre.
+  selector: 'app-heroes', 
+  templateUrl: './heroes.component.html', 
+  styleUrls: ['./heroes.component.css'] 
+})
+
+// Exporta (export) siempre la clase del componente de modo que se pueda importar (import) desde otro punto.
+export class HeroesComponent implements OnInit {
+
+  hero: Hero ={
+    id : 1,
+    name: 'Batman'
+  };
+
+  constructor() { }
+
+  // El ngOnInit es un ‘enganche del ciclo de vida’ (lifecycle hook). 
+  // Angular llama a ngOnInit justo después de crear un componente. 
+  // Es un buen lugar para colocar la lógica de inicialización.
+  ngOnInit() {
+  }
+
+}
