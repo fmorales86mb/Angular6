9e23a3670feab642ed2ea5fcea138388ab57254b
Federico Morales <federico.morales@neoris.com>
2020-01-08 12:25:30 -0300

    Avances sobre ejercicio 02

---
+++ ej02/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  exports:[ RouterModule ]
+  //declarations: [],
+  // imports: [
+  //   CommonModule
+  // ]
+})
+export class AppRoutingModule { }
--- ej02/src/app/app.module.ts
+++ ej02/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
